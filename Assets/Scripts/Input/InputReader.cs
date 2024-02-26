1048c813c5339ac10bce16ed9476bf8a744a1161
NotADev9000 <notadev9000@gmail.com>
2024-02-26 21:35:25 +0000

    Initial commit

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using static GameInput;
+
+[CreateAssetMenu(menuName = "InputReader")]
+public class InputReader : ScriptableObject, IPlayerActions
+{
+    public event Action<Vector2> MoveEvent;
+    public event Action<bool> FireEvent;
+
+    private GameInput _playerInput;
+
+    private void OnEnable()
+    {
+        if (_playerInput == null)
+        {
+            _playerInput = new GameInput();
+            _playerInput.Player.SetCallbacks(this);
+
+            _playerInput.Player.Enable();
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        // noop
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        FireEvent?.Invoke(context.phase == InputActionPhase.Started);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Car Game (WIP)
+
+A physics based car game made in Unity (2022.3.2f1)
