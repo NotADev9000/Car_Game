365d83474f80d99a939093318f54d03858ead0c9
NotADev9000 <notadev9000@gmail.com>
2024-03-01 18:37:48 +0000

    Suspension uses damper value to stabilize vehicle
    * added the average of the surface contact normals. This is used to project onto the plane the vehicle is on

--- Assets/Scripts/HoverVehicleController.cs
+++ Assets/Scripts/HoverVehicleController.cs
@@ -10,10 +10,13 @@ public class HoverVehicleController : MonoBehaviour
 {
     [SerializeField] private InputReader _inputReader;
 
+    [Header("Suspension Settings")]
     [SerializeField] private Transform[] _wheels = Array.Empty<Transform>();
     [SerializeField] private float _suspensionLength = 0.6f;
-    [SerializeField] private float _suspensionForce = 5f;
+    [SerializeField] private float _springStrength = 5f;
+    [SerializeField] private float _springDamper = 2f;
 
+    [Header("Vehicle Settings")]
     [SerializeField] private float _maxSpeed = 25f; // m/s
     [SerializeField] private float _acceleration = 15f;  // m/s^2
     //[SerializeField] private float _rotationSpeed = 60f; // deg/s
@@ -21,6 +24,8 @@ public class HoverVehicleController : MonoBehaviour
     private Rigidbody _rb;
 
     private Vector2 _inputVector;
+
+    private List<Vector3> _suspensionContactNormals = new List<Vector3>();
 
     private void Awake()
     {
@@ -35,16 +40,28 @@ public class HoverVehicleController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        _suspensionContactNormals.Clear();
+
         // SUSPENSION
         Color debugColor;
         RaycastHit hit;
         foreach (Transform wheel in _wheels)
         {
-            if (Physics.Raycast(wheel.position, Vector3.down, out hit, _suspensionLength))
+            if (Physics.Raycast(wheel.position, -wheel.up, out hit, _suspensionLength))
             {
-                Debug.Log("touching: " + wheel.gameObject.name.ToString());
-                float compressionRatio = 1 - (hit.distance/_suspensionLength);
-                _rb.AddForceAtPosition(compressionRatio * Vector3.up * _suspensionForce, wheel.position);
+                float compressionRatio = (float)Math.Round(1 - (hit.distance/_suspensionLength), 2);
+                float springOffsetForce = compressionRatio * _springStrength;
+                
+                Vector3 wheelWorldVelocity = _rb.GetPointVelocity(wheel.position); 
+                float springVelocity = Vector3.Dot(wheel.up, wheelWorldVelocity);
+
+                float force = springOffsetForce - (springVelocity * _springDamper);
+                Debug.Log(force);
+
+                _rb.AddForceAtPosition(force * wheel.up, wheel.position, ForceMode.Acceleration);
+
+                _suspensionContactNormals.Add(hit.normal);
+
                 debugColor = Color.green;
             } 
             else
@@ -52,13 +69,27 @@ public class HoverVehicleController : MonoBehaviour
                 debugColor = Color.red;
             }
 
-            Debug.DrawRay(wheel.position, Vector3.down * _suspensionLength, debugColor);
+            Debug.DrawRay(wheel.position, -wheel.up * _suspensionLength, debugColor);
         }
 
         // MOVEMENT
-        Vector3 force = transform.forward * _inputVector.y * _maxSpeed;
-        force = new Vector3(force.x, _rb.velocity.y, force.z);
-        AccelerateTo(force, _acceleration);
+        Vector3 suspensionContactAverage = Vector3.zero;
+        for (int i = 0; i < _suspensionContactNormals.Count; i++)
+        {
+            suspensionContactAverage += _suspensionContactNormals[i];
+        }
+        if (_suspensionContactNormals.Count > 0) suspensionContactAverage /= _suspensionContactNormals.Count;
+
+        if (_inputVector.y != 0)
+        {
+            Vector3 force = _inputVector.y * _maxSpeed * transform.forward;
+            //force = new Vector3(force.x, _rb.velocity.y, force.z);
+            AccelerateTo(force, _acceleration);
+        }
+        else
+        {
+            Debug.Log(_inputVector.y);
+        }
     }
 
     private void AccelerateTo(Vector3 targetVelocity, float maxAccel)
