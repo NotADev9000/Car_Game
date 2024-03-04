157870ba6182dd24f693704b8a93b062f1d72d5a
NotADev9000 <notadev9000@gmail.com>
2024-03-04 21:28:34 +0000

    Added acceleration & steering
    * movement force is applied at a set distance away from the center of mass
    * movement is projected onto ground before being applied

--- Assets/Scripts/HoverVehicleController.cs
+++ Assets/Scripts/HoverVehicleController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [SelectionBase]
@@ -19,13 +20,19 @@ public class HoverVehicleController : MonoBehaviour
     [Header("Vehicle Settings")]
     [SerializeField] private float _maxSpeed = 25f; // m/s
     [SerializeField] private float _acceleration = 15f;  // m/s^2
-    //[SerializeField] private float _rotationSpeed = 60f; // deg/s
+    [SerializeField] private float _rotationSpeed = 60f; // deg/s
+
+    [Header("Force Settings")]
+    [SerializeField] private float _applyForceForward = 0f;
+    [SerializeField] private float _applyForceDown = 0f;
 
     private Rigidbody _rb;
 
     private Vector2 _inputVector;
 
     private List<Vector3> _suspensionContactNormals = new List<Vector3>();
+    private bool _isGrounded = false;
+    Vector3 applyForceAt;
 
     private void Awake()
     {
@@ -36,6 +43,11 @@ public class HoverVehicleController : MonoBehaviour
     private void OnEnable()
     {
         _inputReader.MoveEvent += OnMove;
+    }
+
+    private void Update()
+    {
+        applyForceAt = transform.position + _rb.centerOfMass + (transform.forward * _applyForceForward) + (-transform.up * _applyForceDown);
     }
 
     private void FixedUpdate()
@@ -56,7 +68,6 @@ public class HoverVehicleController : MonoBehaviour
                 float springVelocity = Vector3.Dot(wheel.up, wheelWorldVelocity);
 
                 float force = springOffsetForce - (springVelocity * _springDamper);
-                Debug.Log(force);
 
                 _rb.AddForceAtPosition(force * wheel.up, wheel.position, ForceMode.Acceleration);
 
@@ -72,35 +83,55 @@ public class HoverVehicleController : MonoBehaviour
             Debug.DrawRay(wheel.position, -wheel.up * _suspensionLength, debugColor);
         }
 
-        // MOVEMENT
+        // grounded if at least 2 wheels are on ground
+        _isGrounded = _suspensionContactNormals.Count > 1;
+
+        // calculate the direction to accelerate the vehicle
+        // this is done to prevent the vehicle moving towards the sky/ground too much when tilted
+        // this is an average of all the surfaces the wheels touch
         Vector3 suspensionContactAverage = Vector3.zero;
         for (int i = 0; i < _suspensionContactNormals.Count; i++)
         {
             suspensionContactAverage += _suspensionContactNormals[i];
         }
-        if (_suspensionContactNormals.Count > 0) suspensionContactAverage /= _suspensionContactNormals.Count;
+        if (_suspensionContactNormals.Count > 0) 
+            suspensionContactAverage = (suspensionContactAverage / _suspensionContactNormals.Count).normalized;
+        Vector3 projectedDirection = Vector3.ProjectOnPlane(transform.forward, suspensionContactAverage);
 
-        if (_inputVector.y != 0)
+        Debug.DrawRay(transform.position, projectedDirection * 3f, Color.green);
+
+        if (_isGrounded)
         {
-            Vector3 force = _inputVector.y * _maxSpeed * transform.forward;
-            //force = new Vector3(force.x, _rb.velocity.y, force.z);
-            AccelerateTo(force, _acceleration);
+            // STEERING
+            if (_rb.velocity.magnitude > 0.0f)
+            {
+                Vector3 steerForce = _inputVector.x * _rotationSpeed * Vector3.up;
+                _rb.AddTorque(steerForce, ForceMode.Acceleration);
+            }
+
+            // MOVEMENT
+            if (_inputVector.y != 0)
+            {
+                Vector3 force = _inputVector.y * _maxSpeed * projectedDirection;
+                force = new Vector3(force.x, _rb.velocity.y, force.z);
+                AccelerateTo(force, _acceleration);
+            }
         }
-        else
-        {
-            Debug.Log(_inputVector.y);
-        }
+
+        Debug.DrawRay(applyForceAt, Vector3.down, Color.magenta);
     }
 
     private void AccelerateTo(Vector3 targetVelocity, float maxAccel)
     {
         Vector3 deltaV = targetVelocity - _rb.velocity;
-        Vector3 accel = deltaV / Time.fixedDeltaTime;
+        Vector3 accel = deltaV / Time.deltaTime;
 
         if (accel.sqrMagnitude > maxAccel * maxAccel)
             accel = accel.normalized * maxAccel;
 
-        _rb.AddForce(accel, ForceMode.Acceleration);
+        //_rb.AddForce(accel, ForceMode.Acceleration);
+
+        _rb.AddForceAtPosition(accel, applyForceAt, ForceMode.Acceleration);
     }
 
     private void OnMove(Vector2 moveVector)
